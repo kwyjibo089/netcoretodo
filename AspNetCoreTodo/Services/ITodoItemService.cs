f0c4ca6a7693500846645ad9af59e7f9eb5636fa
Roman Müller <roman.mueller@swisslife.ch>
2018-10-31 07:47:28 +0100

    initial implementation

---
+++ AspNetCoreTodo/Services/ITodoItemService.cs
@@ -0,0 +1,14 @@
+﻿using AspNetCoreTodo.Models;
+using Microsoft.AspNetCore.Identity;
+using System;
+using System.Threading.Tasks;
+
+namespace AspNetCoreTodo.Services
+{
+    public interface ITodoItemService
+    {
+        Task<TodoItem[]> GetIncompleteItemsAsync(IdentityUser user);
+        Task<bool> AddItemAsync(TodoItem newItem, IdentityUser user);
+        Task<bool> MarkDoneAsync(Guid id, IdentityUser user);
+    }
+}
