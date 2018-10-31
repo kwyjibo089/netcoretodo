f2b6ba142692a04e6f0a8ac1c15b39e155f7cb9f
Roman Müller <roman.mueller@swisslife.ch>
2018-10-31 08:12:48 +0100

    added tests

---
+++ AspNetCoreTodo.IntegrationTests/TestFixture.cs
@@ -0,0 +1,32 @@
+﻿using Microsoft.AspNetCore.Hosting;
+using Microsoft.AspNetCore.TestHost;
+using Microsoft.Extensions.Configuration;
+using System;
+using System.IO;
+using System.Net.Http;
+namespace AspNetCoreTodo.IntegrationTests
+{
+    public class TestFixture : IDisposable
+    {
+        private readonly TestServer _server;
+        public HttpClient Client { get; }
+        public TestFixture()
+        {
+            var builder = new WebHostBuilder()
+            .UseStartup<AspNetCoreTodo.Startup>()
+            .ConfigureAppConfiguration((context, config) =>
+            {
+                config.SetBasePath(Path.Combine(Directory.GetCurrentDirectory(), "..\\..\\..\\..\\AspNetCoreTodo"));
+                config.AddJsonFile("appsettings.json");
+            });
+            _server = new TestServer(builder);
+            Client = _server.CreateClient();
+            Client.BaseAddress = new Uri("http://localhost:8888/Identity/Account/Login?ReturnUrl=%2Ftodo");
+        }
+        public void Dispose()
+        {
+            Client.Dispose();
+            _server.Dispose();
+        }
+    }
+}
